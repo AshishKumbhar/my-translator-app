925e91399e1e6f990640fd894bc417deb70318e7
Ashish Kumbhar <ashishkumbhar@Ashishs-MacBook-Air.local>
2026-04-06 20:24:03 +0200

    initial frontend setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "translatorapp77057ca7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "predictions": {
+    "translateTextAsh": {
+      "region": "string",
+      "sourceLang": "string",
+      "targetLang": "string"
+    }
+  }
+}
